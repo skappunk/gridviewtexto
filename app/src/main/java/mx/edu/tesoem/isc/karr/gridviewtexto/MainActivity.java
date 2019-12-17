5b7acf6465414e01a7af53f2f562b8d73285038a
ISC Tesoem <tesoem@iMac-Tesoem-01.local>
2019-12-17 12:49:40 -0600

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/isc/karr/gridviewtexto/MainActivity.java
@@ -0,0 +1,29 @@
+package mx.edu.tesoem.isc.karr.gridviewtexto;
+
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.widget.ArrayAdapter;
+        import android.widget.GridView;
+
+        import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    GridView gvdatos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gvdatos = findViewById(R.id.gvdatos);
+        String[] arreglo = new String[]{"num1", "num2", "num3", "num4",
+                "num5", "num6", "num7", "num8",
+                "num9", "num10", "num11", "num12",
+                "num13", "num14", "num15", "num16",
+                "num17", "num18", "num19", "num20"};
+
+        ArrayAdapter<String> adapter= new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,arreglo);
+        gvdatos.setAdapter(adapter);
+    }
+}
